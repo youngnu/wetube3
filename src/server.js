3f8d34be8e315ae51947a7d072ff306bb18baaa9
youngnu <qyahzn2004@gmail.com>
2023-09-04 21:58:44 +0900

    app.use & app.get

--- src/server.js
+++ src/server.js
@@ -2,7 +2,31 @@ import express from "express";
 
 const app = express();
 const PORT = 4000;
+
+
+const Middleware = (req, res, next) => {
+    console.log("PATH", req.path)
+    next()
+};
+
+const LoggerMiddle = (req, res, next) => {
+    console.log("METHOD", req.method)
+    next()
+};
+
+const handleHoem = (req, res) => {
+    res.send("I'm in Home😘")
+};
+const handleLogin = (req, res) => {
+    res.send("I'm Login🤣")
+};
+
+app.use(Middleware, LoggerMiddle);
+app.get("/", handleHoem);
+app.get("/login", handleLogin);
+
+
 const handleListening = () => {
-    console.log(`✅this is our server http://localhost:${PORT}`)
+    console.log(`✅Server listening My port on http://localhost:${PORT}`)
 }
 app.listen(PORT, handleListening);
