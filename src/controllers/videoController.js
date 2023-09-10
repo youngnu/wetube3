54490f165806342851f3f149f396c5d9c8321e6b
youngnu <qyahzn2004@gmail.com>
2023-09-10 16:59:30 +0900

    hompage revision and make middleware in Video model

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,44 +1,44 @@
-import Video from "../models/Video"
+import Video from "../models/Video";
 
 export const handleHoem = async (req, res) => {
     const videos = await Video.find({});
-    return res.render("home", {pageTitle: "HOME", videos})
+    return res.render("home", {pageTitle: "HOME", videos});
 };
 
 export const handleWatchVideo = async (req, res) => {
-    const {id} = req.params
-    const video = await Video.findById(id)
-    return res.render("watch", {pageTitle: `Watching ${video.title} video`, video})
+    const {id} = req.params;
+    const video = await Video.findById(id);
+    return res.render("watch", {pageTitle: video.title, video});
 };
 
 export const search = (req, res) => {
-    return res.render("search", {pageTitle: "Search"})
-}
+    return res.render("search", {pageTitle: "Search"});
+};
 
 export const getEdit = async (req, res) => {
-    const {id} = req.params
-    const video = await Video.findById(id)
-    return res.render("edit", {pageTitle: "Editing", video})
-}
+    const {id} = req.params;
+    const video = await Video.findById(id);
+    return res.render("edit", {pageTitle: "Editing", video});
+};
 
 export const postEdit = async (req, res) => {
-    const {id} = req.params
-    const {title, description, hashtags} = req.body
+    const {id} = req.params;
+    const {title, description, hashtags} = req.body;
     await Video.findByIdAndUpdate(id, {
         title,
         description,
-        hashtags
-    })
-    return res.redirect("/")   
-}
+        hashtags: hashtags.split(",").map((word) => (word.startsWith("#")? word : `#${word}`)),
+    });
+    return res.redirect("/");
+};
 
 export const getUpload = (req, res) => {
-    return res.render("upload", {pageTitle: "Upload Video"})
-}
+    return res.render("upload", {pageTitle: "Upload Video"});
+};
 
 export const postUpload = async (req, res) => {
     const { title, description, hashtags } = req.body;
-    const video = await Video.create({
+    await Video.create({
         title,
         description,
         hashtags,
@@ -46,10 +46,9 @@ export const postUpload = async (req, res) => {
             views: 0
         }
     });
-    console.log(video);
-    return res.redirect("/")
-}
+    return res.redirect("/");
+};
 
 export const handleVideoDelete = (req, res) =>{
     res.send("Delete Videos 😎")
-}
+};
--- src/models/Video.js
+++ src/models/Video.js
@@ -4,10 +4,14 @@ const videoSchema = new mongoose.Schema({
     title: { type: String, required: true, trim: true},
     description: { type: String, required: true, trim: true},
     createdAt: { type: Date, required: true, default: Date.now},
-    hashtags: [{ type: String, tirm: true}],
+    hashtags: [{ type: String, tirm: true }],
     meta: {
         views: {type: Number, default: 0, required: true}
     }
+})
+
+videoSchema.pre("save", async function(){
+    this.hashtags = this.hashtags[0].split(",").map((word)=>(word.startsWith("#")? word : `#${word}`))
 })
 
 const Video = mongoose.model("Video", videoSchema)
