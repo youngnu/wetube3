00c58ccd52bc79366ac72f6fa1d437a9a0173ec0
youngnu <qyahzn2004@gmail.com>
2023-09-12 01:08:19 +0900

    status code / try ~ catch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,20 +6,28 @@ export const getJoin = (req, res) => {
 
 export const postJoin = async (req, res) => {
     const {name, email, username, password, password2 } = req.body;
-    const userExist = await User.find({ $or :[{ username}, {email}]})
+    const usernameExist = await User.exists({username})
+    const emailExist = await User.exists({email})
     if(password !== password2){
         return res.render("join", {pageTitle: "Join", errorMessage: "Password not Match"})
     }
-    if(userExist){
-        return res.render("join", {pageTitle: "Join", errorMessage: "user already exist"})
+    if(usernameExist){
+        return res.status(400).render("join", {pageTitle: "Join", errorMessage: "username already exist"})
     };
-    await User.create({
-        name,
-        email,
-        username,
-        password,
-    })
-    return res.redirect("/login")
+    if(emailExist){
+        return res.status(400).render("join", {pageTitle: "Join", errorMessage: "email already exist"})
+    };
+    try{
+        await User.create({
+            name,
+            email,
+            username,
+            password,
+        })
+        return res.redirect("/login")
+    }catch(error){
+        return  res.status(400).render("join", {pageTitle: "Join", errorMessage : error._message})
+    }
 };
 
 export const getLogin = (req, res) =>{
