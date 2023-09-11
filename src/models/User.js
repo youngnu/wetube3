72854e012a6e850443f1df558b6b1ef36be7a8f2
youngnu <qyahzn2004@gmail.com>
2023-09-12 00:19:07 +0900

    password hash using bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     email: {type: String, requried:true, unique:true},
     username: {type: String, required:true, unique:true},
     password: {type: String, required:true},
     name: {type: String, required:true}
+});
+
+userSchema.pre("save", async function() {
+    this.password = await bcrypt.hash(this.password, 5)
 });
 
 const User = mongoose.model("User", userSchema);
