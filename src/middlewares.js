3cfdb7f1e36891b666137b640461dc74df8f30af
youngnu <qyahzn2004@gmail.com>
2023-09-21 00:55:45 +0900

    locals in pug header

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,6 @@
 export const localMiddleware = (req, res, next) => {
     console.log(req.session)
     res.locals.loggedIn = Boolean(req.session.loggedIn)
-    res.locals.loggedInuUser = req.session.user
+    res.locals.loggedInUser = req.session.user
     next()
 }
