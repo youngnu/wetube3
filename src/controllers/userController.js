40b3d5eefc029fce3dd286f3ad9dd2f6d69d9801
youngnu <qyahzn2004@gmail.com>
2023-09-21 18:21:29 +0900

    startGithubLogin / params / URLSearchParams().toString

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -50,6 +50,19 @@ export const postLogin = async (req, res) =>{
     return res.redirect("/")
 }
 
+export const startGithubLogin = (req, res) => {
+    const baseUrl =  "https://github.com/login/oauth/authorize"
+    const config = {
+        client_id: "8508aa87809e77889bfa",
+        allow_signup: false,
+        scope: "read:user user:email"
+    }
+    const params = new URLSearchParams(config).toString()
+    const finalUrl = `${baseUrl}?${params}`
+    return res.redirect(finalUrl)
+}
+
+
 export const handleLogout = (req, res) => {
     return res.send("I'm Logout 🤣")
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { handleLogout, handleUserEdit } from "../controllers/userController";
+import { handleLogout, handleUserEdit, startGithubLogin } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", handleLogout)
 userRouter.get("/edit", handleUserEdit)
+userRouter.get("/github/start", startGithubLogin)
 
 export default userRouter
