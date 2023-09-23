55c1847660e8d433ad220882f48bbc3c92b4cce5
youngnu <qyahzn2004@gmail.com>
2023-09-24 01:23:27 +0900

    protect middleware revise

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { handleWatchVideo, getUpload, postUpload, getEdit, postEdit, deleteVideo } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
-videoRouter.route("/upload").get(getUpload).post(postUpload)
+videoRouter.route("/upload").all(protectorMiddleware).get(getUpload).post(postUpload)
 videoRouter.get("/:id([0-9a-f]{24})", handleWatchVideo)
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit)
 videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo)
