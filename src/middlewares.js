de1066cc945050045c2ca52868d30b0ff739a2d4
youngnu <qyahzn2004@gmail.com>
2023-09-24 01:20:18 +0900

    protect middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,4 +2,20 @@ export const localMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn)
     res.locals.loggedInUser = req.session.user
     next()
-}
+};
+
+export const protectorMiddleware = (req, res, next) => {
+    if(!req.session.loggedIn){
+        return res.redirect("/")
+    } else {
+        return next()
+    }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+    if(!req.session.loggedIn){
+        return next()
+    } else {
+        return res.redirect("/")
+    }
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { handleLogout, getEdit, postEdit, startGithubLogin, finshGithubLogin, getChangePassword, postChangepassword } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", handleLogout)
-userRouter.route("/edit").get(getEdit).post(postEdit)
-userRouter.get("/github/start", startGithubLogin)
-userRouter.get("/github/finish", finshGithubLogin)
-userRouter.route("/change-password").get(getChangePassword).post(postChangepassword)
+userRouter.get("/logout", protectorMiddleware, handleLogout)
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit)
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin)
+userRouter.get("/github/finish", publicOnlyMiddleware, finshGithubLogin)
+userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangepassword)
 
 export default userRouter
