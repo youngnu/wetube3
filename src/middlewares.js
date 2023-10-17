7961f7ddb5552ad6e5b33177981428ef227eb299
youngnu <qyahzn2004@gmail.com>
2023-10-18 00:49:10 +0900

    forms scss

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 export const localMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn)
-    res.locals.loggedInUser = req.session.user
+    res.locals.loggedInUser = req.session.user || {}
     next()
 };
 
