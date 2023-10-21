9df55b571e3e34140d1f57db846fd431eb2fe815
youngnu <qyahzn2004@gmail.com>
2023-10-21 17:27:34 +0900

    videoPlayer playbutton and mutebutton create

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,29 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumeRange = document.getElementById("volume");
+
+
+
+const handlePlayClick = () => {
+    if(video.paused) {
+        video.play()
+    } else {
+        video.pause()
+    };
+    playBtn.innerText = video.paused ? "Play" : "Pause";
+}
+
+const handleMute = () => {
+    if(video.muted){
+        video.muted = false
+    } else {
+        video.muted = true
+    };
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : 0.5;
+}
+
+playBtn.addEventListener("click", handlePlayClick)
+muteBtn.addEventListener("click", handleMute)
