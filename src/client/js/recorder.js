c962cfd9260ca6ce0c49c3727cca2906ae9b6547
youngnu <qyahzn2004@gmail.com>
2023-10-25 01:01:07 +0900

    new MediaRecorder(stream) / ondataavailable / recorder.start() / recorder.stop()

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,6 +1,14 @@
 const startBtn = document.getElementById("startBtn")
 const video = document.getElementById("preview")
 
+let stream;
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording"
+    startBtn.removeEventListener("click",handleStop)
+    startBtn.addEventListener("click", handleStart)
+}
+
 
 const handleStart = async() => {
     stream = await navigator.mediaDevices.getUserMedia({
@@ -8,6 +16,15 @@ const handleStart = async() => {
     });
     video.srcObject = stream;
     video.play()
+    startBtn.innerText = "Stop Recording"
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop)
+    const recorder = new MediaRecorder(stream)
+    recorder.ondataavailable = (event) => console.log("event", event)
+    console.log("event1", recorder);
+    recorder.start();
+    console.log("event2", recorder);
+    recorder.stop();
 }
 
 startBtn.addEventListener("click", handleStart);
