7b9fcd5b4c03a3f5aa314dcc394c524b649189ee
youngnu <qyahzn2004@gmail.com>
2023-10-25 01:05:47 +0900

    global variable let recorder

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,11 +2,13 @@ const startBtn = document.getElementById("startBtn")
 const video = document.getElementById("preview")
 
 let stream;
+let recorder
 
 const handleStop = () => {
     startBtn.innerText = "Start Recording"
     startBtn.removeEventListener("click",handleStop)
     startBtn.addEventListener("click", handleStart)
+    recorder.stop();
 }
 
 
@@ -19,12 +21,11 @@ const handleStart = async() => {
     startBtn.innerText = "Stop Recording"
     startBtn.removeEventListener("click", handleStart);
     startBtn.addEventListener("click", handleStop)
-    const recorder = new MediaRecorder(stream)
+    recorder = new MediaRecorder(stream)
     recorder.ondataavailable = (event) => console.log("event", event)
     console.log("event1", recorder);
     recorder.start();
     console.log("event2", recorder);
-    recorder.stop();
 }
 
 startBtn.addEventListener("click", handleStart);
