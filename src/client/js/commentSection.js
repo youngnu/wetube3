ca0706923b3c9b8f20aaeaa9e63f7e9ffe10bed9
youngnu <qyahzn2004@gmail.com>
2023-10-30 19:20:11 +0900

    fetch is request backend something

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,10 +1,16 @@
+//videoContainer for dataset video._id 
+const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 const textarea = document.querySelector("textarea");
 const btn = document.querySelector("button")
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(textarea.value)
+    const videoId = videoContainer.dataset.id;
+    const text = textarea.value;
+    fetch(`/api/videos/${videoId}/comment`, {
+        method: "POST"
+    });
 }
 
 
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -108,3 +108,7 @@ export const registerView = async (req, res) => {
     await video.save();
     return res.sendStatus(200);
 }
+
+export const createComment = (req, res) => {
+    return res.redirect("/")
+}
