16c948d816b745bc3469f9739e3e63cf49c7fedb
youngnu <qyahzn2004@gmail.com>
2023-10-31 00:28:58 +0900

    createComment handler / new mongoose.Schema({}) / mongoose.model() / Comment.create({})

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -8,6 +8,9 @@ const handleSubmit = (event) => {
     event.preventDefault();
     const videoId = videoContainer.dataset.id;
     const text = textarea.value;
+    if(text === ""){
+        return;
+    }
     fetch(`/api/videos/${videoId}/comment`, {
         method: "POST",
         //express에게 우리가 보내는 것이 json이라고 알려줘야 한다.
@@ -17,6 +20,7 @@ const handleSubmit = (event) => {
         //object로 보내면 controller에서 req.body로 접근이 불가능하기에, JSON.Stringify({})로 객체정보를 전달
         body: JSON.stringify({ text })
     });
+    textarea.value= "";
 }
 
 
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
 import Video from "../models/Video";
 import User from "../models/User"
+import Comment from "../models/Comment"
 
 export const handleHoem = async (req, res) => {
     const videos = await Video.find({}).sort({createdAt: "desc"}).populate("owner");
@@ -109,8 +110,20 @@ export const registerView = async (req, res) => {
     return res.sendStatus(200);
 }
 
-export const createComment = (req, res) => {
-    console.log(req.params);
-    console.log(req.body);
-    return res.end()
+export const createComment = async (req, res) => {
+    const {
+        params: {id},
+        body: {text},
+        session: {user}
+    } = req;
+    const video = await Video.findById(id);
+    if(!video){
+        return res.sendStatus(404);
+    }
+    const comment = await Comment.create({
+        text,
+        owner: user._id,
+        video: id
+    })
+    return res.sendStatus(201);
 }
