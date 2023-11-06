2e9618a331b2e269eaff043ee4f137e69f79e86d
youngnu <qyahzn2004@gmail.com>
2023-11-06 23:17:01 +0900

    ffmpeg.webasm install

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,5 @@
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -7,6 +9,7 @@ let videoFile;
 
 const handleDownload = () => {
     const a = document.createElement("a");
+    //videoFile은 ㅣet으로 광역함수 되어 있으며, handleStart에서 URL.createObjectURL()을 이용하여 handleDownload로 넘겨짐
     a.href = videoFile;
     a.download = "MyRecording.webm";
     document.body.appendChild(a);
@@ -23,18 +26,24 @@ const handleStop = () => {
     recorder.stop();
 };
 
-//ondataavailable은 recoder.stop()뒤에 만들어진 data를 이용가능하게 해준다. recorder.ondataavailable함수가 실행되면서 Blob이란 파일이 나온다.
+//ondataavailable은 recoder.stop()뒤에 만들어진 data를 이용가능하게 해준다. recorder.ondataavailable함수가 실행되면서 Blob이란 data파일(event.data)이 나온다.
 //evet.data를 통해 Blob event를 받고, URL.creatObjcetURL(event.data)를 통해서 URL을 만들어주면 녹화파일을 사용할 수 있게 된다.
 const handleStart = async() => {
     stream = await navigator.mediaDevices.getUserMedia({
         audio: false, video: {width: 1024, height: 576}
     });
+    console.log("stream", stream);
+    //srcObject는 보통 JS에서 미디어 요스 (audio, video)의 소스를 지정하는데 사용되는 소석입니다. 
+    //이것은 미디어 요소에 미디어 스트림, 오디오 또는 비디오파일의 URL을 설정하고 재생할 때 사용합니다.
     video.srcObject = stream;
     video.play();
+    console.log("video.srcObject", video.srcObject);
     startBtn.innerText = "Stop Recording";
     startBtn.removeEventListener("click", handleStart);
     startBtn.addEventListener("click", handleStop);
     recorder = new MediaRecorder(stream, { mimeType: "video/webm"});
+    //ondataavailable은 MediaRecorder과 함께 사용되는 경우가 많으며, 미디어 스트림을 처리하거나 미디어 데이터를 수신하고 처리할때 사용됨
+    //recorder.stop()이 실행되면 그 event를 받아 밑의 코드가 실행된다.
     recorder.ondataavailable = (event) => {
         console.log("event", event);
         console.log("evet.data", event.data);
@@ -52,9 +61,7 @@ const handleStart = async() => {
         video.loop = true 
         video.play()
     }
-    console.log("event1", recorder);
     recorder.start();
-    console.log("event2", recorder);
 };
 
 startBtn.addEventListener("click", handleStart);
