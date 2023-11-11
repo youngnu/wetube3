ec7750872112db0eb3762d5fe81d1f1b2e4d7019
youngnu <qyahzn2004@gmail.com>
2023-11-11 23:32:12 +0900

    process.env.DB_URL

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ app.use(
         secret: process.env.DB_SECRET,
         resave: false,
         saveUninitialized: false,
-        store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube3"})
+        store: MongoStore.create({ mongoUrl: process.env.DB_URL})
     })
 );
 // app.use(flash())로 req.flash(type, 내용)형태를 써줄 수 있게된다.
